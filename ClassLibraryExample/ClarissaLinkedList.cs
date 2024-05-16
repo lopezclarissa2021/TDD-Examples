5df4a06b53a0bc8f86ac43c3d5a649ababadf9a7
Clarissa Lopez <lopezclarissa2021@gmail.com>
2024-05-16 15:04:25 -0700

    fixing errors in the code

--- ClassLibraryExample/ClarissaLinkedList.cs
+++ ClassLibraryExample/ClarissaLinkedList.cs
@@ -1,30 +1,119 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibraryExample
 { // node class for a linked list
-    public class Node
+  // Node class for a linked list item
+    public class Node<T>
     {
-        public string Data { get; set; }
+        public T Data { get; set; }
+        public Node<T>? Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
     }
 
     // the linked list class itself
-    public class ClarissaLinkedList
+    public class ClarissaLinkedList<T>
     {
-        public Node Head { get; set; }
+        public Node? Head { get; set; }
+       
 
         public ClarissaLinkedList() { 
-            Head = new Node();
+            Head = null;
         }
        
         public string Get(int  index)
         {
-            return Head.Data;
+            Node node = Head;
+            int counter = 0;
+
+            // search through the node list to find the item at the given index (if it exists).
+            while (counter != index)
+            {
+                node = node.Next;
+                counter++;
+            }
+
+            return node.Data;
         }
 
-        public void Add(string item)
+        public void AddToBack(string item)
         {
-            Head = new Node { Data = item };
+            // create a new node to hold our new item
+            Node newNode = new Node(item);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+
+            Node temp = FindEndOfList();
+
+            temp.Next = newNode;
+        }
+
+        public Node FindEndOfList()
+        {
+            Node position = Head;
+
+            while (position.Next != null)
+            {
+                position = position.Next;
+            }
+
+            return position;
+        }
+
+        public Node FindMiddleBrute()
+        {
+            Node ptr = Head;
+
+            int counter = 0;
+
+            while (ptr != null)
+            {
+                ptr = ptr.Next;
+                counter++;
+            }
+
+            ptr = Head;
+
+            for (int i = 0; i < counter / 2; i++)
+            {
+                ptr = ptr.Next;
+            }
+
+            return ptr;
+        }
+
+        public Node FindMiddleTortiseHare()
+        {
+            Node slow_ptr = Head;
+            Node fast_ptr = Head;
+
+            while (fast_ptr != null && fast_ptr.Next != null)
+            {
+                slow_ptr = slow_ptr.Next;
+                fast_ptr = fast_ptr.Next.Next;
+            }
+
+            return slow_ptr;
+        }
+
+        public void AddToFront(string item)
+        {
+            Node newNode = new Node(item);
+
+            newNode.Next = Head;
+            Head = newNode;
         }
     }
 }
--- LibraryTests/ClarissaLinkedListTests.cs
+++ LibraryTests/ClarissaLinkedListTests.cs
@@ -6,43 +6,114 @@ namespace LibraryTests
     public class ClarissaLinkedListTests
     {
 
-        // Creating a singly linked list that accepts strings
-        /*
-        shopping list
-
-        bread
-        milk 
-        cheese
-        */
 
         [TestMethod]
-        
-        public void LinkedListAcceptsStrigns_returnsStringData()
+        [DataRow("Toast")]
+        public void LinkedListAcceptsStrigns_returnsStringData(string input)
         {
             // arrange
             ClarissaLinkedList list = new ClarissaLinkedList();
 
             // act
-            string result = list.Get(1);
+            list.AddToBack(input);
+            string result = list.Get(0);
 
             // assert
             Assert.IsInstanceOfType(result, typeof(string));
         }
 
         [TestMethod]
-        [DataRow("Toast", typeof(Node)]
-        public void LinkedListAcceptsAnItem_HeadReturnsTheItem(string input)
+        [DataRow("Toast")]
+        public void LinkedListAcceptsAnItem_HeadReturnsTheData(string input)
+        {
+            
+            // arrange
+            ClarissaLinkedList list = new ClarissaLinkedList();
+            Node expected = new Node(input);
+
+            // act
+            list.AddToBack(input);
+
+            Node result = list.Head;
+
+            // assert
+            Assert.AreEqual(result.Data, expected.Data);
+        }
+
+        [TestMethod]
+        [DataRow("Toast")]
+        public void LinkedListAcceptsAnItem_CreatesANodeInTheHead(string input)
+        {
+
+            // arrange
+            ClarissaLinkedList list = new ClarissaLinkedList();
+
+
+            // act
+            list.AddToBack(input);
+
+            // assert
+            Assert.IsInstanceOfType(list.Head, typeof(Node));
+        }
+
+        [TestMethod]
+        public void WhenLinkedListIsCreated_HeadISNull()
+        {
+
+            // arrange
+            ClarissaLinkedList list = new ClarissaLinkedList();
+
+
+            // act
+            Node? result = list.Head;
+
+            // assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [DataRow("Foo", "Bar", "Baz")]
+        [DataRow("lake", "picnic", "sun")]
+        public void WhenLinkedListGetsMultipleItems_ItemsAreInCorrectOrder(string one, string two, string three)
+        {
+
+            // arrange
+            ClarissaLinkedList list = new ClarissaLinkedList();
+
+
+            // act
+            list.Add(one);
+            list.Add(two); 
+            list.Add(three);
+
+            string? resultOne = list.Get(0);
+            string? resultTwo = list.Get(1);
+            string? resultThree = list.Get(2);
+
+            // assert
+            Assert.AreEqual(resultOne, one);
+            Assert.AreEqual(resultTwo, two);
+            Assert.AreEqual(resultThree, three);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(10, 20, 3)]
+        [DataRow(-1, 20, -3)]
+        public void WhenLinkedListGetsIntegers_IntegersAreReturned(int one, int two, int three)
         {
             // arrange
             ClarissaLinkedList list = new ClarissaLinkedList();
 
             // act
-            // add the input item to the list
-            list.Add(input);
-            Node result = list.Head;
+            list.AddToBack(one);
+            list.AddToBack(two);
+            list.AddToBack(three);
+
+            int result = list.Get(0);
 
             // assert
-            Assert.AreEqual(result, new Node() { Data = "Toast"});
+            Assert.AreEqual(result, one);
         }
     }
 }
